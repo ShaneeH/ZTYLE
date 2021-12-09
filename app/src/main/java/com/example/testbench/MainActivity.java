eeec89ae32bdc8fe86029079832c704551d711cb
ShaneeH <hahesyshane@gmail.com>
2021-12-09 09:24:06 +0000

    Initial commit - Ztyle

---
+++ app/src/main/java/com/example/testbench/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.testbench;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+
+    }
+
+    public void goToHome(View v){
+
+        Intent i = new  Intent(this, Home.class);
+        startActivity(i);
+
+
+
+    }
+}
